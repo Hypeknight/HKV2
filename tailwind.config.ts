bc32cb2084a9cccee18a72b164df278103f37576
Hypeknight <willie_gerald1@yahoo.com>
2026-04-06 14:22:54 -0500

    updt2

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,8 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    './app/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
+    '.app/**/*.{ts,tsx}',
+    '.components/**/*.{ts,tsx}',
     '.lib/**/*.{ts,tsx}'
   ],
   theme: {
