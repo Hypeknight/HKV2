3aed0599535b71ea7712cfe9069085ec918ee3a0
Hypeknight <willie_gerald1@yahoo.com>
2026-04-09 18:55:45 -0500

    nav imp

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -61,7 +61,7 @@ export default function Navbar({
           <>
             <Link href="/dashboard">Dashboard</Link>
 
-            {(role === 'admin' || role === 'venue_owner') && (
+            {(role === 'admin') &&(
               <Link href="/admin">Admin</Link>
             )}
 
