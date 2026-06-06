20b181bcce06cc65c9b0a1798d938a8124b478e7
Hypeknight <willie_gerald1@yahoo.com>
2026-06-05 19:37:53 -0500

    second try

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -108,7 +108,7 @@ export default async function EventsPage() {
     .eq('status', 'active')
     .eq('is_public', true)
     .is('removed_at', null)
-    //.gte('event_start_at', now)
+    .gte('event_start_at', now)
     .order('event_start_at', { ascending: true });
 
   if (hypeError) {
@@ -119,7 +119,7 @@ export default async function EventsPage() {
     .from('external_events')
     .select('*')
     .eq('status', 'active')
-    .gte('event_start_at', now)
+    //.gte('event_start_at', now)
     .order('event_start_at', { ascending: true });
 
   if (externalError) {
